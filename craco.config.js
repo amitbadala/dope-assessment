eb0dedcd32027c02dfeaaddb765e5ea9b8b9b6ed
amitbadala <amitbadala07@gmail.com>
2023-08-24 18:04:21 +0530

    Make craco config work with less styles

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,17 @@
+const CracoLessPlugin = require("craco-less");
+
 module.exports = {
-  webpack: {
-    alias: {},
-    plugins: [],
-    configure: (webpackConfig, { env, paths }) => {
-      const oneOfLocation = webpackConfig.module.rules.findIndex((rule) =>
-        Boolean(rule.oneOf)
-      );
-      const rules = webpackConfig.module.rules[oneOfLocation].oneOf;
-      rules.push({
-        test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"],
-      });
-      return webpackConfig;
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            strictMath: true,
+            noIeCompat: true,
+          },
+        },
+      },
     },
-  },
+  ],
 };
