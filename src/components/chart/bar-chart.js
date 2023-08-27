d15ffbbbd976b6b95fa3fd9e82b18c62f998ed4a
amitbadala <amitbadala07@gmail.com>
2023-08-27 23:59:25 +0530

    Update chart and add table header

--- src/components/chart/bar-chart.js
+++ src/components/chart/bar-chart.js
@@ -2,8 +2,15 @@
 import { Bar } from "react-chartjs-2";
 import { CategoryScale } from "chart.js";
 import Chart from "chart.js/auto";
+import "./bar-chart.less";
 
 Chart.register(CategoryScale);
+
+const colors = [
+  "#13A877", // Color for the first bar
+  "#DE9E6A", // Color for the second bar
+  "#CF6262", // Color for the third bar
+];
 
 export const BarChart = ({ data }) => {
   const chartData = {
@@ -12,19 +19,43 @@ export const BarChart = ({ data }) => {
       {
         label: "App Usage Volume",
         data: data.map((item) => item.userGain),
-        backgroundColor: "rgba(75,192,192,0.6)",
-        borderColor: "rgba(75,192,192,1)",
+        backgroundColor: colors,
+        borderColor: colors.map((color) => color.replace("0.6", "1")), // Assuming you're using rgba colors
         borderWidth: 1,
-        hoverBackgroundColor: "rgba(75,192,192,0.8)",
-        hoverBorderColor: "rgba(75,192,192,1)",
+        hoverBackgroundColor: colors.map((color) =>
+          color.replace("0.6", "0.8")
+        ),
+        hoverBorderColor: colors.map((color) => color.replace("0.6", "1")),
       },
     ],
   };
 
+  const chartOptions = {
+    scales: {
+      x: {
+        ticks: {
+          color: "white", // Color for x-axis labels
+        },
+      },
+      y: {
+        ticks: {
+          color: "white", // Color for y-axis labels
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        labels: {
+          color: "white", // Color for legend labels
+        },
+      },
+    },
+  };
+
   return (
     <div className="chart-container">
-      <h2 style={{ textAlign: "center" }}>Bar Chart</h2>
-      <Bar data={chartData} />;
+      <h2 style={{ textAlign: "center", color: "white" }}>Bar Chart</h2>
+      <Bar data={chartData} options={chartOptions} />;
     </div>
   );
 };
--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,5 +1,4 @@
 import Table from "../../components/table/table";
-import { useEffect, useState } from "react";
 import { fetchTableData, chartData } from "../../service/dashboard-service";
 import { BarChart } from "../../components/chart/bar-chart";
 import StatsCard from "../../components/stats/stats-card";
@@ -9,36 +8,30 @@ import { useQuery } from "react-query";
 import "./dashboard.less";
 
 const Dashboard = () => {
-  const [tableData, setTableData] = useState([]);
   const { data, error, isLoading } = useQuery("tableData", fetchTableData);
 
-  //   useEffect(() => {
-  //     getTableData();
-  //   }, []);
-
-  //   const getTableData = async () => {
-  //     let result = await fetchTableData();
-  //     setTableData(result);
-  //   };
-
   return (
-    <>
-      <div className="dashboard-container">
-        <div className="grid-50">
-          <div className="flex flex-column space-between">
-            <StatsCard totalUsers={34} /> <SampleForm />
-          </div>
-          <BarChart data={chartData} />
+    <div className="dashboard-container">
+      <div className="grid-50">
+        <div className="flex flex-column space-between">
+          <StatsCard totalUsers={34} /> <SampleForm />
         </div>
-        <Table
-          className="table table-bordered"
-          columns={columnsConfig}
-          isLoading={isLoading}
-          error={error}
-          data={data}
-        />
+        <BarChart data={chartData} />
       </div>
-    </>
+      <div className="table-header">
+        <h4>App Usage</h4>
+        <div>
+          <input placeholder="Search Data" type="text"></input>
+        </div>
+      </div>
+      <Table
+        className="table table-bordered"
+        columns={columnsConfig}
+        isLoading={isLoading}
+        error={error}
+        data={data}
+      />
+    </div>
   );
 };
 
